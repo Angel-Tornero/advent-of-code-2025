faf4310c4553ed110f7e6aa1b56c9f63ff92dbb4
Angel-Tornero <angeltornerohdez@gmail.com>
2025-12-05 00:07:52 +0100

    add: day 3 - lobby

--- day_3_lobby/src/Main.java
+++ day_3_lobby/src/Main.java
@@ -9,22 +9,25 @@ import java.util.Scanner;
 public class Main {
 
     private static final String INPUT_PATH = "day_3_lobby/src/input";
-    private static final int DIAL_SIZE = 100;
-    private static final int DIAL_INITIAL_POSITION = 50;
+    private static final int BATTERIES_FOR_PART_ONE = 2;
+    private static final int BATTERIES_FOR_PART_TWO = 12;
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(new FileReader(INPUT_PATH))) {
 
-            List<DialRotation> allRotations = new ArrayList<>();
+            List<BatteryBank> allBanks = new ArrayList<>();
 
             while (scanner.hasNext()) {
-                allRotations.add(DialRotation.fromLine(scanner.next()));
+                allBanks.add(BatteryBank.fromLine(scanner.next()));
             }
 
-            Dial dial = new Dial(DIAL_SIZE, DIAL_INITIAL_POSITION);
+            BatterySystem batterySystem = new BatterySystem(allBanks);
 
-            System.out.println(dial.getEndingZeroCounter(allRotations));
-            System.out.println(dial.getAnytimeZeroCounter(allRotations));
+            long firstSolution = batterySystem.getLargestPossibleJoltage(BATTERIES_FOR_PART_ONE);
+            System.out.println(firstSolution);
+
+            long secondSolution = batterySystem.getLargestPossibleJoltage(BATTERIES_FOR_PART_TWO);
+            System.out.println(secondSolution);
 
         } catch (FileNotFoundException exception) {
             System.err.println("Input file not found: " + INPUT_PATH);
